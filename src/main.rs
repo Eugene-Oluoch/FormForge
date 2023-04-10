f243f71dd836cf475a72c893a9cb13e09c1ecb06
Mark Eugene Oluoch <meoluoch@safaricom.co.ke>
2023-04-10 15:44:38 +0300

    Hard reset for main.rs

--- src/main.rs
+++ src/main.rs
@@ -30,8 +30,8 @@ async fn main() {
     let _ =rocket::build()
       .manage(StateCustom::new(client))
       .mount("/", routes![welcome])
-      .mount("/options/", routes![add_option])
-      .mount("/selects/",routes![get_select])
+      .mount("/options/", routes![])
+      .mount("/selects/",routes![])
       .mount("/forms/",routes![])
       .launch()
       .await;
@@ -39,42 +39,37 @@ async fn main() {
 
 #[get("/")]
 async fn welcome() -> String{
-  String::from("Welcome Luck Bustard")
+  String::from("Welcome to FormForge 🙂")
 }
 
-#[get("/<id>")]
-async fn get_select(id:&str,client:&State<StateCustom>){
-  println!("{:?}",get_all::<Select>(&client.client, "crabs_test", "selects", map("select",id)).await);
-}
+// #[post("/add",data="<data>")]
+// async fn add_option(data:Json<OptionSelectReceive>,client:&State<StateCustom>) -> Result<Json<ReturnId>,Json<ReturnError>>{
+//   let mut option = data.0.convert();
 
-#[post("/add",data="<data>")]
-async fn add_option(data:Json<OptionSelectReceive>,client:&State<StateCustom>) -> Result<Json<ReturnId>,Json<ReturnError>>{
-  let mut option = data.0.convert();
-
-  // FORCE ID, IF SUPPLIED ,TO NONE
-  if let Some(id) = option.get_id(){
-    let _ = &option
-    .set_id(None)
-    .build();
-  }
+//   // FORCE ID, IF SUPPLIED ,TO NONE
+//   if let Some(id) = option.get_id(){
+//     let _ = &option
+//     .set_id(None)
+//     .build();
+//   }
 
 
-  // VALIDATE SELECT ID 
-  if let Some(select_id) = option.get_select_id(){
-    let select_requested = get_by_id::<Select>(&client.client,"crabs_test","selects",select_id.to_hex().as_str()).await;
-    if select_requested == None{
-      return Err(Json(ReturnError::new("Select with the given id doesn't exist".to_string())))
-    }
-  }
+//   // VALIDATE SELECT ID 
+//   if let Some(select_id) = option.get_select_id(){
+//     let select_requested = get_by_id::<Select>(&client.client,"crabs_test","selects",select_id.to_hex().as_str()).await;
+//     if select_requested == None{
+//       return Err(Json(ReturnError::new("Select with the given id doesn't exist".to_string())))
+//     }
+//   }
 
-  // ID OF CREATED OPTION
-  let option_id = insert_doc(&client.client, "crabs_test", "options", &option).await.unwrap().inserted_id.as_object_id().unwrap().to_string();
+//   // ID OF CREATED OPTION
+//   let option_id = insert_doc(&client.client, "crabs_test", "options", &option).await.unwrap().inserted_id.as_object_id().unwrap().to_string();
 
-  // UPDATE OPTIONS IN THE SELECT
-  if let Some(select_id) = option.get_select_id(){
-    let document = doc! { "$push": { "options": string_to_object_id(&option_id) } };
-    update_push::<Select>(&client.client, "crabs_test", "selects", document, select_id.to_hex().as_str()).await;
-  }
+//   // UPDATE OPTIONS IN THE SELECT
+//   if let Some(select_id) = option.get_select_id(){
+//     let document = doc! { "$push": { "options": string_to_object_id(&option_id) } };
+//     update_push::<Select>(&client.client, "crabs_test", "selects", document, select_id.to_hex().as_str()).await;
+//   }
 
-  Ok(Json(ReturnId::new(option_id)))
-}
+//   Ok(Json(ReturnId::new(option_id)))
+// }
