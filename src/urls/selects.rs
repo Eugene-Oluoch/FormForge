7d4d21b1cb78382f42f58284761651b3a3cd99bc
Mark Eugene Oluoch <meoluoch@safaricom.co.ke>
2023-04-12 07:59:35 +0300

    Added an endpoint to handle select create

--- src/urls/selects.rs
+++ src/urls/selects.rs
@@ -1,14 +1,38 @@
-use mongodb::bson;
+use mongodb::bson::{self, from_bson};
+use mongodb::error::Error;
 use rocket::State;
+use rocket::serde::json::Json;
 use crate::utils::StateCustom;
-use crate::db::get_all;
+use crate::db::{get_all,insert_doc};
 use crate::{Select,SelectReceive};
 use crate::repository::map;
-
+use uuid::Uuid;
 
 
 #[get("/<id>")]
-pub async fn get_select_by_id(id:&str,client:&State<StateCustom>) -> String{
-  let c = get_all::<Select>(&client.client, "crabs_test", "selects", map("select",id)).await; 
-  serde_json::to_string(&c).expect("Failed to serialise")
+pub async fn get_select_by_id(id:String,client:&State<StateCustom>) -> Result<Json<SelectReceive>,String>{
+  let document = get_all::<Select>(&client.client, "crabs_test", "selects", map("select",id.as_str())).await;
+  if let Ok(doc) = document{
+    Ok(Json(from_bson(bson::Bson::Document(doc)).expect("Failed here")))
+  } else {
+    Err("Select with the given id doesn't exist 🙁".to_string())
+  }
+}
+/* 
+
+*/
+#[post("/add",data="<data>")]
+pub async fn add_select(data:Json<SelectReceive>,client:&State<StateCustom>) -> Result<&str, &str>{
+  let mut select = data.0;
+  
+  // GENERATE AN ID FOR THE SELECT FIELD
+  select.set_id(Uuid::new_v4().to_string());
+
+
+  let results = insert_doc(&client.client, "crabs_test", "selects", &select).await;
+  if let Ok(result) = results{
+    Ok("Select Added successfully 🙂")
+  } else {
+    Err("Failed to create the select field 🙁")
+  }
 }
