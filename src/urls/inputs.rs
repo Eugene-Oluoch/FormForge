dd3be5d6a66cedab68ea5823d0a7a5c34bdc256a
Mark Eugene Oluoch <meoluoch@safaricom.co.ke>
2023-04-14 21:43:44 +0300

    Added an endpoint to get input

--- src/urls/inputs.rs
+++ src/urls/inputs.rs
@@ -1,12 +1,24 @@
 use rocket::State;
 use rocket::serde::json::Json;
 use crate::models::{input::Input, traits::ResetDefaults};
-use crate::db::{insert_doc};
+use crate::db::{insert_doc,get_by_id};
 use crate::utils::StateCustom;
+use crate::utils::ReturnError;
+
+#[get("/<id>")]
+pub async fn get_input<'a>(id:&str,client:&State<StateCustom>) -> Result<Json<Input>,Json<ReturnError<'a>>>{
+  let input_data = get_by_id::<Input>(&client.client, "crabs_test", "inputs", id).await.expect("Failed");
+  if let Some(result) = input_data{
+    Ok(Json(result))
+  }else{
+    Err(Json(ReturnError::new("Input with the provided id doesn't exists. 🙁")))
+  }
+}
 
 #[post("/add", data="<data>")]
 pub async fn add_input(data:Json<Input>,client:&State<StateCustom>){
   let mut input = data.0;
   let _ = &mut input.reset();
-  insert_doc(&client.client, "crabs_test", "inputs", &input).await;
+  let _ = &mut input.map_type();
+  let _ = insert_doc(&client.client, "crabs_test", "inputs", &input).await;
 }
