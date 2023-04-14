381587a898d85eda8a14f5dcd04170c1910816bd
Mark Eugene Oluoch <meoluoch@safaricom.co.ke>
2023-04-14 19:40:09 +0300

    Found an optimal way to handle validations

--- src/types/validation/mod.rs
+++ src/types/validation/mod.rs
@@ -1,27 +1,58 @@
-/*
-FIELDS Excluding checkbox and radio (TO BE HANDLED DIFFERENTLY)
-* Month, Time, Week 
+use serde::{Serialize, Deserialize};
 
-Validation Rules For Input types 
-* Number | Range  {
-  minlength,
-  maxlength,
-  required
-}
-
-* Url | Password | Text | Email | Tel {
-  minlength,
-  maxlength,
-  pattern,
-  required
-}
-
-* Month | Time | Week | Date | Datelocaltime {
-  min -> string mapping,
-  max -> string mapping,
-  required
-}
-
-
-
+/* 
+Chechbox and Radio to be handled differently
 */
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Validate{
+  #[serde(skip_serializing_if = "Option::is_none")]
+  pub min:Option<i32>,
+  #[serde(skip_serializing_if = "Option::is_none")]
+  pub max:Option<i32>,
+  #[serde(skip_serializing_if = "Option::is_none")]
+  pub maxlength:Option<String>,
+  #[serde(skip_serializing_if = "Option::is_none")]
+  pub minlength:Option<String>,
+  #[serde(skip_serializing_if = "Option::is_none")]
+  pub required:Option<bool>,
+  #[serde(skip_serializing_if = "Option::is_none")]
+  pub pattern:Option<String>,
+}
+
+
+impl Validate{
+  pub fn new() -> Self{
+    Self {
+      min:None,
+      max:None,
+      maxlength:None,
+      minlength:None,
+      required:Some(false),
+      pattern:None,
+    }
+  }
+
+  pub fn filter_validation(&mut self,match_type:&str) -> &Self{
+    match match_type {
+      "number" | "range" => {
+        self.maxlength = None;
+        self.minlength = None;
+        self.pattern = None;
+        self
+      },
+      "url" | "password" | "text" | "email" | "tel" => {
+        self.minlength = None;
+        self.maxlength = None;
+        self
+      },
+      "month" | "time" | "week" | "date" | "datelocal-time" => {
+        self.min = None;
+        self.max = None;
+        self.pattern = None;
+        self
+      },
+      _=>todo!()
+    }
+  }
+
+}
