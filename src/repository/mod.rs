6cac50b092241554710d87afa517779e98040836
Mark Eugene Oluoch <meoluoch@safaricom.co.ke>
2023-04-22 18:17:42 +0300

    Upgraded the mongo pipeline

--- src/repository/mod.rs
+++ src/repository/mod.rs
@@ -1,4 +1,3 @@
-pub mod pipelines;
 use std::collections::HashMap;
 use mongodb::{bson::{ doc, Document}};
 
@@ -10,80 +9,100 @@ pub fn map(name:&str, id:&str) -> Vec<Document>{
 fn pipelines (id:&str) -> HashMap<&str,Vec<Document>>{
 
   // FORM PIPELINE FOR FETCHING FORM AND ALL REFERENCE RELATIONSHIP
+  // TODO FIX PIPELINE NOT RETURNING SELECTS WHICH HAVE ONE OPTION SET ARCHIVE SET TO TRUE
   let form_pipeline = vec![
     doc! {
-      "$match": {
-          "_id": id
-      }
-    },
-    doc! {
-      "$lookup": {
-        "from": "inputs",
-        "localField": "inputs",
-        "foreignField": "_id",
-        "as": "inputs"
-      }
-    },
-    doc! {
-      "$lookup": {
-      "from": "selects",
-      "localField": "selects",
-      "foreignField": "_id",
-      "as": "selects"
-    }
-    },
-    doc! {
-      "$unwind": {
-          "path": "$selects",
-          "preserveNullAndEmptyArrays": true
-      }
-  },
-  doc! {
-      "$match": {
-          "selects": { "$ne": [] }
-      }
-  },
-  doc! {
-      "$lookup": {
-          "from": "options",
-          "localField": "selects.options",
-          "foreignField": "_id",
-          "as": "selects.options"
-      }
-  },
-  doc! {
-      "$group": {
-          "_id": "$_id",
-          "name": { "$first": "$name" },
-          "steps": { "$first": "$steps" },
-          "archive": { "$first": "$archive" },
-          "updated_at": { "$first": "$updated_at" },
-          "created_at": { "$first": "$created_at" },
-          "inputs": {
-              "$first": "$inputs"
-          },
-          "selects": {
-              "$push": "$selects"
-          }
-      }
-  }
-];
-  // SELECT PIPELINE FOR FETCHING FORM AND ALL REFERENCE RELATIONSHIP
-  let select_pipeline = vec![
-    doc! {
-      "$match": {
-        "_id": id
-    }
+        "$match": {
+            "_id": id
+        }
     },
     doc! {
         "$lookup": {
-        "from": "options",
-        "localField": "options",
-        "foreignField": "_id",
-        "as": "options"
-      }
+            "from": "inputs",
+            "let": { "inputs": "$inputs" },
+            "pipeline": [
+                { "$match": {
+                    "$expr": { "$and": [
+                        { "$in": [ "$_id", "$$inputs" ] },
+                        { "$ne": [ "$archive", true ] }
+                    ]}
+                }}
+            ],
+            "as": "inputs"
+        }
     },
+    doc! {
+        "$lookup": {
+            "from": "selects",
+            "let": { "selects": "$selects" },
+            "pipeline": [
+                { "$match": {
+                    "$expr": { "$and": [
+                        { "$in": [ "$_id", "$$selects" ] },
+                        { "$ne": [ "$archive", true ] }
+                    ]}
+                }},
+                { "$lookup": {
+                    "from": "options",
+                    "localField": "options",
+                    "foreignField": "_id",
+                    "as": "options"
+                }},
+                { "$match": {
+                    "options.archive": { "$ne": true }
+                }}
+            ],
+            "as": "selects"
+        }
+    },
+    doc! {
+        "$unwind": {
+            "path": "$selects",
+            "preserveNullAndEmptyArrays": true
+        }
+    },
+    doc! {
+        "$group": {
+            "_id": "$_id",
+            "name": { "$first": "$name" },
+            "steps": { "$first": "$steps" },
+            "archive": { "$first": "$archive" },
+            "updated_at": { "$first": "$updated_at" },
+            "created_at": { "$first": "$created_at" },
+            "inputs": {
+                "$first": "$inputs"
+            },
+            "selects": {
+                "$push": "$selects"
+            }
+        }
+    }
   ];
+
+  // SELECT PIPELINE FOR FETCHING FORM AND ALL REFERENCE RELATIONSHIP
+  let select_pipeline = vec![
+    doc! {
+        "$match": {
+            "_id": id
+        }
+    },
+    doc! {
+        "$lookup": {
+            "from": "options",
+            "let": { "options": "$options" },
+            "pipeline": [
+                { "$match": {
+                    "$expr": { "$and": [
+                        { "$in": [ "$_id", "$$options" ] },
+                        { "$ne": [ "$archive", true ] }
+                    ]}
+                }}
+            ],
+            "as": "options"
+        }
+    }
+];
+
 
   let mut pipeline_value = HashMap::new();
   let _ = &mut pipeline_value.insert("select", select_pipeline);
