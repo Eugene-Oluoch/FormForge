8ab5f718c5d58bbf2bf15b2d484edf8b1b50260b
Mark Eugene Oluoch <meoluoch@safaricom.co.ke>
2023-04-27 20:32:05 +0300

    populates update

--- src/urls/textarea.rs
+++ src/urls/textarea.rs
@@ -4,12 +4,14 @@ use crate::{
     textarea::{TextArea}
   },
   utils::{
-    StateCustom, ReturnError
+    StateCustom, ReturnError, ReturnMessage, ReturnErrors, ReturnId
   },
   views::{
     textarea::{
       get_textarea_view,
-      add_textarea_view
+      add_textarea_view,
+      delete_textarea_view,
+      update_textareas_view
     }
   }
 };
@@ -21,14 +23,16 @@ pub async fn get_textarea_by_id<'a>(id:&'a str,client:&'a State<StateCustom>) ->
 
 
 #[post("/add",data="<data>")]
-pub async fn add_textarea(data:Json<TextArea>,client:&State<StateCustom>){
-  add_textarea_view(data.0, &client.client).await;
+pub async fn add_textarea(data:Json<TextArea>,client:&State<StateCustom>) -> Result<Json<ReturnId>, Json<ReturnError>>{
+  add_textarea_view(data, &client.client).await
 }
 
 #[put("/<id>",data="<data>")]
-pub async fn update_textarea<'a>(id:&'a str,data:Json<TextArea>,client:&'a State<StateCustom>){
+pub async fn update_textarea<'a>(id:&'a str,data:Json<TextArea>,client:&'a State<StateCustom>) -> Result<Json<ReturnMessage<'a>>,Json<ReturnErrors>>{
+  update_textareas_view(id, data.0, &client.client).await
 }
 
 #[delete("/<id>")]
-pub async fn delete_textarea<'a>(id:&str,client:&State<StateCustom>){
+pub async fn delete_textarea<'a>(id:&str,client:&State<StateCustom>) -> Result<Json<ReturnMessage<'a>>,Json<ReturnError<'a>>>{
+  delete_textarea_view(id, &client.client).await
 }
